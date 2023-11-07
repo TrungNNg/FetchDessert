07eee30f68590b82933bf98bc24937d245b662bc
Trung Nguyen <nhattrungnguyen14@yahoo.com>
2023-11-07 10:01:23 -0500

    Initial Commit

---
+++ Dessert/DessertApp.swift
@@ -0,0 +1,17 @@
+//
+//  DessertApp.swift
+//  Dessert
+//
+//  Created by Trung Nguyen on 11/7/23.
+//
+
+import SwiftUI
+
+@main
+struct DessertApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
